4ffed44c58c8419dc5bdf7a1d62bea4f5deecf64
toniebe <toniebe48@gmail.com>
2022-03-09 12:09:21 +0700

    Tugas-Harian-3

---
+++ Tugas-3-Looping/looping.js
@@ -0,0 +1,28 @@
+// soal no 1
+console.log("LOOPING PERTAMA");
+var x = 2;
+while (x <= 20) {
+  console.log(x + " - I love Coding");
+  x += 2;
+}
+
+console.log("LOOPING KEDUA");
+var y = 20;
+while (y >= 2) {
+  console.log(y + " - I will become mobile developer");
+  y -= 2;
+}
+
+console.log("----------------------------");
+
+// soal no 2
+
+for(let i = 1; i <= 20; i ++) {
+  if(i % 3 === 0 && i % 2 === 1) {
+    console.log(i + " - I Love Coding");
+  } else if (i % 2 === 0) {
+    console.log(i + " - Berkualitas");
+  } else {
+    console.log(i + " - Santai");
+  }
+}
