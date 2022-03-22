2417463f68436ac87901a9d558b474fa48f01951
toniebe <toniebe48@gmail.com>
2022-03-22 11:29:03 +0700

    Tugas-Harian-12

--- Tugas/Tugas-ReactNative/SanberApp/App.js
+++ Tugas/Tugas-ReactNative/SanberApp/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Telegram from './Telegram';
+import Movie from './Tugas 12/Movie';
 import About from './Tugas11/About';
 import Login from './Tugas11/Login';
 import Register from './Tugas11/Register';
@@ -11,7 +12,8 @@ export default function App() {
     {/* <Telegram /> */}
     {/* <Login /> */}
     {/* <Register /> */}
-    <About />
+    {/* <About /> */}
+    <Movie />
     </>
   );
 }
